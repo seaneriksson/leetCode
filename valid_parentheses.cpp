abb06dbb8a58c40970ed4c40ed9c81fc13d36da3
seaneriksson <46273556+seaneriksson@users.noreply.github.com>
2022-05-12 15:24:16 -0400

    Update valid_parentheses.cpp

--- valid_parentheses.cpp
+++ valid_parentheses.cpp
@@ -7,97 +7,38 @@ using namespace std;
 
 int main()
 {
-    string s = "({{{{}}}))";
-      
-    int length = s.size();
-    char list[2][length];
+    stack <char> stack;
+    int len = s.size();
+
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            stack.push(s[i]);
+        else if (s[i] == ')' || s[i] == ']' || s[i] == '}')
+        {
+            if(stack.empty())
+                return false;
+            else if(s[i] == ')' && stack.top() != '(')
+                return false;
+            else if(s[i] == ']' && stack.top() != '[')
+                return false;
+            else if(s[i] == '}' && stack.top() != '{')
+                return false;
+            else
+                stack.pop();
+        }
+    }
     
-
-
-    if (length % 2 != 0)
+    if(stack.empty())
     {
-        return false;
-
-    }
-
-    //create a 2d array
-    for (int i = 0; i < length; i++)
-    {
-        list[0][i] = s[i];
-        list[1][i] = '-';
-    }
-
-    
-    int pairCount = 0;
-
-    char charIndex;
-    char charNeeded;
-    int relCount;
-
-    //search for duplicate
-    for (int i = 0; i < length; i++)
-    {
-        charIndex = s[i];
-
-        
-
-        if (charIndex == '(')
-        {
-            charNeeded = ')';
-        }
-
-        if (charIndex == '[')
-        {
-            charNeeded = ']';
-        }
-
-        if (charIndex == '{')
-        {
-            charNeeded = '}';
-        }
-
-        cout << "charIndex" << i << " = " << charIndex << "\n";
-
-        relCount = 1;
-        
-        for (int j = i+1; j < length; j++)
-        {
-            
-            cout << "charFound" << j << " = " << s[j] << "\n";
-            cout << "pairCount " << pairCount << "\n";
-
-            if (charNeeded == s[j] && relCount % 2 == 0)
-            {
-                cout << "Pair exist " << charIndex << charNeeded << " " << i << ", " << j << "\n\n";
-                list[1][i] = 'x';
-                list[1][j] = 'x';
-                pairCount = pairCount + 2;
-                charNeeded = '?';
-                break;
-            }
-            relCount++;
-        }
-
-    }
-
-    //print out the 2d array
-    for (int i = 0; i < length; i++)
-    {
-       cout << list[0][i] << ", " << list[1][i] << "\n";
-
-    }
-
-    if (pairCount == length)
-    {
-        cout << "Valid parentheses \n" << pairCount;
+        cout << "valid \n";
         return true;
     }
-    else 
-    {   
-        cout << "Not valid parentheses \n" << pairCount;
+    else
+    {
+        cout << "not valid \n";
         return false;
     }
-    
     
 
     return 0;
